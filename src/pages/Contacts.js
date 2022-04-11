2e43e9c9d55601e51183203907de5328acf04795
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-04-11 15:44:52 +0600

    change styles

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,8 +1,20 @@
+import ImgLogo from "../assets/header.jpg"
+
 function Contacts() {
   return (
     <article>
       <h1>This is Contacts page.</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex pariatur voluptatum doloribus enim repudiandae ullam sequi quo suscipit voluptas id, laboriosam necessitatibus, dolore corrupti. Explicabo iure doloribus illum repudiandae tenetur.</p>
+      <img src={ImgLogo} alt=""/>
+      <p className="line">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa praesentium fuga ipsa similique omnis explicabo earum libero porro, fugiat exercitationem enim deserunt iusto iste assumenda ratione, necessitatibus, vel asperiores tempora.</p>
+    <p>Sit atque sequi aspernatur consectetur autem fugiat. Voluptatum debitis, inventore corrupti similique repellat, placeat cumque obcaecati deserunt culpa nemo ex delectus nostrum quis labore iste maiores accusantium commodi molestiae velit.</p>
+    <p>Sint, quis facere facilis perferendis eligendi hic recusandae excepturi quos quisquam modi? Qui, consectetur praesentium. Incidunt consectetur, minus earum iure at itaque sed? Esse delectus laudantium iure eius velit consectetur.</p>
+    <p>Vel est voluptates aspernatur, expedita corrupti tenetur asperiores tempore nobis numquam aliquid provident mollitia inventore quas impedit consequuntur quis facere quam labore. Asperiores similique laborum natus consequuntur molestias exercitationem. Deleniti.</p>
+    <p>Assumenda obcaecati temporibus eos velit dicta optio ipsa, sint facilis. Quidem ducimus in aspernatur harum ullam nihil, velit quo, vel non accusamus recusandae minima voluptates commodi beatae. Consequatur, voluptas officiis!</p>
+    <p>Quae, dolorum itaque dolor possimus voluptate ullam vel ad animi magni iste fugiat accusantium sunt? Corrupti inventore autem dolore obcaecati, maiores maxime unde natus quibusdam dolor, consequatur praesentium voluptas qui.</p>
+    <p>Debitis facilis voluptate illo autem sed corrupti nostrum! Provident, veritatis et? Quis exercitationem corporis eius nulla soluta. Eaque consequatur velit voluptatibus. Saepe amet alias, consequuntur sed numquam repudiandae praesentium ad.</p>
+    <p>Sunt cupiditate nam earum veniam voluptate expedita doloribus, ut ipsum sed incidunt libero quam amet autem obcaecati architecto qui a accusamus facere dolorum ratione. Culpa nisi veniam ducimus exercitationem molestiae.</p>
+    <p>Soluta id vitae molestias quas, saepe, harum pariatur consectetur distinctio debitis vel eveniet perspiciatis quia voluptate at illo, veritatis labore tempore. Quos nam fuga eligendi ex in quae quas id?</p>
+    <p>Suscipit quam fugit voluptatum, facilis, tempore, iusto ratione qui aspernatur totam officiis ut temporibus a soluta veritatis vero doloremque id consequuntur laborum esse modi quaerat! Magnam consequuntur magni aliquam accusantium!</p>
     </article>
   );
 }

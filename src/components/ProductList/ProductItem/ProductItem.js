2e9ac4c1ad7422da20ba0d8b64bbe20a4083f585
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-05-16 11:45:52 +0600

    change styles

--- src/components/ProductList/ProductItem/ProductItem.js
+++ src/components/ProductList/ProductItem/ProductItem.js
@@ -4,11 +4,11 @@ import classes from "./ProductItem.module.css";
 function ProductItem({ product }) {
   return (
     <div className={classes.ProductItem}>
-      <img src={product.image} alt={product.title} />
+      <img src={product.image}  />
       <Link to={"/products/" + product.productId}>{product.title}</Link>
       <div className={classes.price}>${product.price}</div>
     </div>
   );
 }
-
+// alt={product.title}
 export default ProductItem;

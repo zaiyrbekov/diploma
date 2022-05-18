5b8ef3c2391d91a7bbee332e95615252064e4c76
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-05-18 15:41:22 +0600

    add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,25 @@
-import ImgLogo1 from "../assets/header.jpg"
+
+import headerImage from "../assets/header6.jpg";
 import Header from "../components/Header/Header";
+
+// import CategoryList from "../components/CategoryList/CategoryList";
+// import { getCategories } from "../data/categories";
+
+
+
 
 function Home() {
   return (
-   <>
-   <Header
+    <>
+      <Header
    title="This is home page."
-   image={ImgLogo1}>
+   image={headerImage}>
      
        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam obcaecati quasi reiciendis, facere quis, quibusdam sint optio, voluptatibus corrupti consequatur sed qui itaque cupiditate. Incidunt quibusdam dicta blanditiis dolorum in?
    </Header>
-   </>
+      {/* <CategoryList categories={getCategories()}/> */}
+    </>
   );
 }
 
-export default Home
+export default Home;

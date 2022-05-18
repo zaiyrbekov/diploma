02f18c53764957d8d25265cbcf74757ef1648166
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-05-18 15:40:34 +0600

    add

--- src/components/ProductList/ProductItem/ProductItem.js
+++ src/components/ProductList/ProductItem/ProductItem.js
@@ -4,9 +4,12 @@ import classes from "./ProductItem.module.css";
 function ProductItem({ product }) {
   return (
     <div className={classes.ProductItem}>
-      <img src={product.image}  />
+      <img src={product.image} alt={product.title} />
+      <div className={classes.ProductFlax}>
       <Link to={"/products/" + product.productId}>{product.title}</Link>
+      <p>{product.lorem}</p>
       <div className={classes.price}>${product.price}</div>
+      </div>
     </div>
   );
 }

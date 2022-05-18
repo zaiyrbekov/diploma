38bd15a0d822670218d2c6c8dbac293481262482
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-05-18 15:41:05 +0600

    add

--- src/data/products.js
+++ src/data/products.js
@@ -4,54 +4,160 @@ import productImage3 from "../assets/photo3.jpg";
 import productImage4 from "../assets/photo4.jpg";
 import productImage5 from "../assets/photo5.jpg";
 import productImage6 from "../assets/photo6.jpg";
+import productImage7 from "../assets/photo7.jpg";
+import productImage8 from "../assets/photo8.jpg";
+
+
+import drinks1 from "../assets/drink1.jpg";
+import drinks2 from "../assets/drink2.jpg";
+import drinks3 from "../assets/drink3.jpg";
+import drinks4 from "../assets/drink4.jpg";
 
 
 const products = [
   {
     image: productImage1,
-    productId: "inner world of oil",
-    title: "inner world of oil",
+    productId: "Beefy Burgers",
+    title: "Beefy Burgers",
+    categoryId: "Burgers",
+    lorem: "Great way to make your business appear trust and relevant.",
     description: `When the fruit is ripe, it is almost always handpicked, using either "selective picking", where only the ripe fruit is removed, or "strip-picking", where all of the fruit is removed from a limb all at once. Selective picking is often used to produce higher quality coffee because the cherries are picked at their ripest. Strip-picking is indiscriminate and will harvest unripe, ripe, and over-ripe fruit. To improve quality after strip-picking, the harvest must be sorted.`,
-    price: 10,
+    price: 6,
   },
   {
     image: productImage2,
-    productId: "classic koloboks",
-    title: "classic koloboks",
+    productId: "Burger Boys",
+    title: "Burger Boys",
+    categoryId: "Burgers",
+    lorem: "Great way to make your business appear trust and relevant.",
     description: `The Asian palm civet eats coffee berries and excretes the beans. Because the civet prefers the taste of the ripest cherries, the civet selectively harvests the cherries. Its digestive system then processes the beans by breaking down the mucilage and pulp surrounding the seed. Once the seeds are excreted by the civet, they can be harvested, processed and sold as a niche product. Once they are finally processed, these beans are called kopi luwak, and are often marketed as a rare and expensive coffee.`,
-    price: 12,
+    price: 5,
   },
   {
     image: productImage3,
-    productId: "spicy koloboks",
-    title: "spicy koloboks",
+    productId: "Burger Bizz",
+    title: "Burger Bizz",
+    categoryId: "Burgers",
+    lorem: "Great way to make your business appear trust and relevant.",
     description: `Two methods are primarily used to process coffee berries. The first, "wet" or "washed" process, has historically usually been carried out in Central America and areas of Africa. The flesh of the cherries is separated from the seeds and then the seeds are fermented – soaked in water for about two days. This softens the mucilage, which is a sticky pulp residue that is still attached to the seeds. Then this mucilage is washed off with water.`,
-    price: 14,
+    price: 5,
   },
   {
     image: productImage4,
-    productId: "smoked koloboks",
-    title: "smoked koloboks",
+    productId: "Crackles Burger",
+    title: "Crackles Burger",
+    categoryId: "Burgers",
+    lorem: "Great way to make your business appear trust and relevant.",
     description: `The "dry processing" method, cheaper and simpler, was historically used for lower-quality beans in Brazil and much of Africa, but now brings a premium when done well. Twigs and other foreign objects are separated from the berries and the fruit is then spread out in the sun on concrete, bricks or raised beds for 2–3 weeks, turned regularly for even drying.`,
-    price: 13,
+    price: 6,
   },
   {
     image: productImage5,
-    productId: "classic tulip",
-    title: "classic tulip",
+    productId: "Bull Burgers",
+    title: "Bull Burgers",
+    categoryId: "Burgers",
+    lorem: "Great way to make your business appear trust and relevant.",
     description: `Clinical research indicates that moderate coffee consumption is benign or mildly beneficial as a stimulant in healthy adults, with continuing research on whether long-term consumption reduces the risk of some diseases, although some of the long-term studies are of questionable credibility.`,
-    price: 11,
+    price: 4,
   },
   {
     image: productImage6,
-    productId: "burning pills",
-    title: "burning pills",
+    productId: "Rocket Burgers",
+    title: "Rocket Burgers",
+    categoryId: "Burgers",
+    lorem: "Great way to make your business appear trust and relevant.",
     description: `Clinical research indicates that moderate coffee consumption is benign or mildly beneficial as a stimulant in healthy adults, with continuing research on whether long-term consumption reduces the risk of some diseases, although some of the long-term studies are of questionable credibility.`,
-    price: 11,
+    price: 5,
   },
+  {
+    image: productImage7,
+    productId: "Smokin Burger",
+    categoryId: "Burgers",
+    title: "Smokin Burger",
+    lorem: "Great way to make your business appear trust and relevant.",
+    description: `Clinical research indicates that moderate coffee consumption is benign or mildly beneficial as a stimulant in healthy adults, with continuing research on whether long-term consumption reduces the risk of some diseases, although some of the long-term studies are of questionable credibility.`,
+    price: 6,
+  },
+  {
+    image: productImage8,
+    productId: "Delish Burger",
+    categoryId: "Burgers",
+    title: "Delish Burger",
+    lorem: "Great way to make your business appear trust and relevant.",
+    description: `Clinical research indicates that moderate coffee consumption is benign or mildly beneficial as a stimulant in healthy adults, with continuing research on whether long-term consumption reduces the risk of some diseases, although some of the long-term studies are of questionable credibility.`,
+    price: 5,
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  {
+    image: drinks1,
+    productId: "Coca-Cola",
+    title: "Coca-Cola",
+    categoryId: "Drinks",
+    lorem: "Great way to make your business appear trust and relevant.",
+    description: `Clinical research indicates that moderate coffee consumption is benign or mildly beneficial as a stimulant in healthy adults, with continuing research on whether long-term consumption reduces the risk of some diseases, although some of the long-term studies are of questionable credibility.`,
+    price: 4,
+  },
+  {
+    image: drinks2,
+    productId: "Pepsi",
+    title: "Pepsi",
+    categoryId: "Drinks",
+    lorem: "Great way to make your business appear trust and relevant.",
+    description: `Clinical research indicates that moderate coffee consumption is benign or mildly beneficial as a stimulant in healthy adults, with continuing research on whether long-term consumption reduces the risk of some diseases, although some of the long-term studies are of questionable credibility.`,
+    price: 5,
+  },
+  {
+    image: drinks3,
+    productId: "Fanta",
+    categoryId: "Drinks",
+    title: "Fanta",
+    lorem: "Great way to make your business appear trust and relevant.",
+    description: `Clinical research indicates that moderate coffee consumption is benign or mildly beneficial as a stimulant in healthy adults, with continuing research on whether long-term consumption reduces the risk of some diseases, although some of the long-term studies are of questionable credibility.`,
+    price: 6,
+  },
+  {
+    image: drinks4,
+    productId: "Sprite",
+    categoryId: "Drinks",
+    title: "Sprite",
+    lorem: "Great way to make your business appear trust and relevant.",
+    description: `Clinical research indicates that moderate coffee consumption is benign or mildly beneficial as a stimulant in healthy adults, with continuing research on whether long-term consumption reduces the risk of some diseases, although some of the long-term studies are of questionable credibility.`,
+    price: 5,
+  },
+
+
+
+  
 ];
 
-export function getProducts() {
+export function getProducts(categoryId) {
+  if (categoryId) {
+    return products.filter(product => product.categoryId === categoryId);    
+  }
+
   return products;
 }
 

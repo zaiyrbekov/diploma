68a85bd0fa811211e016971daacc9b3c9c6eee7e
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-05-19 14:38:02 +0600

    Add

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import ImgLogo from "../assets/header5.jpg"
+import Header from "../components/Header/Header";
+function About() {
+  return (
+    
+     <>
+   <Header
+   title="This is About page."
+   image={ImgLogo}>
+     
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam obcaecati quasi reiciendis, facere quis, quibusdam sint optio, voluptatibus corrupti consequatur sed qui itaque cupiditate. Incidunt quibusdam dicta blanditiis dolorum in?
+   </Header>
+   </>
+  );
+}
+
+export default About;

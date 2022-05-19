f2bf6073a650e0dc7b5c9438e51bb8183f2e870a
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-05-19 14:37:38 +0600

    Add

---
+++ src/components/ui/NavIcon/NavIcon.js
@@ -0,0 +1,31 @@
+import "./NavIcon.module.css";
+
+function NavIcon() {
+  return (
+    <ul className="fox">
+      <li>
+        <a href="#">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+      </li>
+
+      <li>
+        <a className="num" href="#">
+          +10 367 453 7382
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavIcon;

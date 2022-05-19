de0abd68cc3279a2779719dde7d804e0aedf45d4
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-05-19 14:38:13 +0600

    Add

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -3,7 +3,7 @@
 
 
 import { useParams } from "react-router-dom";
-import Header from "../components/Header/Header";
+// import Header from "../components/Header/Header";
 import { getProduct } from "../data/products";
 
 function Product() {
@@ -15,13 +15,21 @@ function Product() {
   }
 
   return (
-    <>
-      <Header
-        title={product.title}
-        image={product.image}>
-        {product.description}
-      </Header>
+   
+       <>
+      <div style={{display: "flex", padding: "3rem"}}>
+        <img style={{width: "450px", marginRight: "1rem", borderRadius: "10px"}} src={product.image} alt={product.title} />
+        <div style={{padding: "1rem", fontFamily: "serif", textAlign: "center"}}>
+          <h2 style={{marginTop: "0"}}>{product.title}</h2>
+          <p  style={{fontSize: "1.2rem"}}>{product.description}</p>
+          <button 
+            style={{cursor: "pointer", padding: "0.5rem", fontSize: "1rem", background: "#0b1c2c", color: "white", fontFamily: "serif", border: "solid 1px grey", borderRadius: "5px"}}>
+            Add to cart - $ {product.price}
+          </button>
+        </div>
+      </div>
     </>
+    
   );
 }
 

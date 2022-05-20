4651e1928ab28f32cfeda70d0e70a8a2ab90cd3f
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-05-20 15:15:25 +0600

    Add

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -5,10 +5,10 @@ function Contacts() {
     
      <>
    <Header
-   title="This is Contacts page."
+   title="GET IN TOUCH."
    image={ImgLogo}>
      
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam obcaecati quasi reiciendis, facere quis, quibusdam sint optio, voluptatibus corrupti consequatur sed qui itaque cupiditate. Incidunt quibusdam dicta blanditiis dolorum in?
+       
    </Header>
    </>
   );

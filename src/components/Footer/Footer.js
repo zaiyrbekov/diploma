5de541966014f63ea0eaad0bd2b187887dbff7a1
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-05-20 08:20:56 +0600

    Add

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,31 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="Footer">
-      <div className="container">Fresh Kurut 2022</div>
+     
+      <ul>
+        <li>
+          <a href="#">
+            <i class="fab fa-instagram"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fab fa-twitter"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fab fa-google-plus-g"></i>
+          </a>
+        </li>
+      </ul>
+      <div className="footer-logo">2022 All rights reserved | This template is made
+          with <i class="far fa-heart"></i> by </div>
     </footer>
   );
 }

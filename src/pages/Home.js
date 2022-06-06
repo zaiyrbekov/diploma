94b7486f801b73ccc453b40a8b2274ddd86dc60b
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-06 13:37:59 +0600

    add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,33 @@
-
+import { Link } from "react-router-dom";
 import headerImage from "../assets/header6.jpg";
 import Header from "../components/Header/Header";
 
+import ProductList from "../components/ProductList/ProductList";
+
+import { getProducts } from "../data/products";
+
 // import CategoryList from "../components/CategoryList/CategoryList";
 // import { getCategories } from "../data/categories";
-
-
 
 
 function Home() {
   return (
     <>
       <Header
-   title="Burger 
+        title="Burger 
    Bachelor"
-   image={headerImage}>
-     
-       {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam obcaecati quasi reiciendis, facere quis, quibusdam sint optio, voluptatibus corrupti consequatur sed qui itaque cupiditate. Incidunt quibusdam dicta blanditiis dolorum in? */}
-   </Header>
+        image={headerImage}
+      >
+        {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam obcaecati quasi reiciendis, facere quis, quibusdam sint optio, voluptatibus corrupti consequatur sed qui itaque cupiditate. Incidunt quibusdam dicta blanditiis dolorum in? */}
+      </Header>
       {/* <CategoryList categories={getCategories()}/> */}
+      <div class="section-title ">
+        <span>Burger Menu</span>
+        <h3>Best Ever Burgers</h3>
+      </div>
+      <ProductList products={getProducts("Burgers")} />
+      <Link class="boxed-btn5" to={"/products/"}>More Items</Link>
+      
     </>
   );
 }

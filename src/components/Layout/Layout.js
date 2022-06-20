07e99b682aee02d64540c7d10b06fac83227f9e5
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-20 11:05:29 +0600

    Add

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,16 @@ import { useState } from "react";
 import Drawer from "../Drawer/Drawer";
 import Footer from "../Footer/Footer";
 import Toolbar from "../Toolbar/Toolbar";
-
+import { useHistory } from "react";
 function Layout({ children }) {
   const [drawerOpen, setDrawerOpen] = useState(false);
 
+  // const history = useHistory();
+  // history.listen((location, action) => {
+  //   if (drawerOpen) {
+  //     setDrawerOpen(false);
+  //   }
+  // });
   function toggleDrawer() {
     setDrawerOpen(!drawerOpen);
   }
@@ -14,9 +20,7 @@ function Layout({ children }) {
     <main className="Layout">
       <Toolbar toggleDrawer={toggleDrawer} />
       <Drawer open={drawerOpen} toggle={toggleDrawer} />
-      <div className="container">
-        {children}
-      </div>
+      <div className="container">{children}</div>
       <Footer />
     </main>
   );

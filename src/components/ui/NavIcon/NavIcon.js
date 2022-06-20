49da4bad10bb949ea47d7be36af56fe3a58354a2
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-20 11:05:40 +0600

    Add

--- src/components/ui/NavIcon/NavIcon.js
+++ src/components/ui/NavIcon/NavIcon.js
@@ -4,25 +4,26 @@ function NavIcon() {
   return (
     <ul className={classes.fox}>
       <li>
-        <a href="/">
+        <a className={classes.tool} href="/">
           <i className="fab fa-instagram"></i>
         </a>
       </li>
       <li>
-        <a href="/">
+        <a className={classes.tool} href="/">
           <i className="fab fa-twitter"></i>
         </a>
       </li>
       <li>
-        <a href="/">
+        <a className={classes.tool} href="/">
           <i className="fab fa-facebook-f"></i>
         </a>
       </li>
       <li>
-        <a href="/">
+        <a className={classes.tool} href="/">
           <i className="fab fa-google-plus-g"></i>
         </a>
       </li>
+     
       <li>
         <a className={classes.number} href="/">
           +10 367 453 7382

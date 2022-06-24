006f3ec609a7c17d5ce9b6a664ad7161c5c8d1a4
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-24 15:15:39 +0600

    Create new component CartButton and added button Add to cart

---
+++ src/components/CartButton/CartButton.js
@@ -0,0 +1,15 @@
+import { useDispatch } from "react-redux";
+import { add } from "../../redux/cartSlice";
+
+export default function CartButton({ productId }) {
+  const dispatch = useDispatch();
+
+  function onAddToCart() {
+    dispatch(add(productId));
+  }
+
+  return (
+    <button className="CartButton" onClick={onAddToCart}>Add to card</button>
+    
+  );
+}

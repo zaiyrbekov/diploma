da2c9bd4b0f9d2ff2a31b285a272dd2169161dc1
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-24 15:15:10 +0600

    added Dispatch for app js and use in useEffect

---
+++ src/redux/cartSlice.js
@@ -0,0 +1,61 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const checkout = createAsyncThunk('cart/checkout', async (order, thunkAPI) => {
+  const response = await axios.post('https://diploma-d5005-default-rtdb.firebaseio.com/orders.json', order);
+
+  return response.data;
+});
+
+function saveInStorage(items) {
+  localStorage.setItem('cartItems', JSON.stringify(items));
+}
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: {
+    },
+  },
+  reducers: {
+    add: (store, action) => {
+      if (store.items[action.payload]) {
+        store.items[action.payload]++;
+      }
+      else {
+        store.items[action.payload] = 1;
+      }
+      saveInStorage(store.items);
+    },
+    remove: (store, action) => {
+      delete store.items[action.payload];
+      saveInStorage(store.items);
+    },
+    increment: (store, action) => {
+      store.items[action.payload]++;
+      saveInStorage(store.items);
+    },
+    decrement: (store, action) => {
+      if (store.items[action.payload] > 1) {
+        store.items[action.payload]--;
+      }
+      else {
+        delete store.items[action.payload];
+      }
+      saveInStorage(store.items);
+    },
+    restore: (store, action) => {
+      store.items = JSON.parse(localStorage.getItem('cartItems') ?? '{}');
+    }
+  },
+  extraReducers: {
+    [checkout.fulfilled]: (state, action) => {
+      console.log(action);
+      state.items = {};
+    }
+  }
+});
+
+export const { add, remove, increment, decrement } = cartSlice.actions;
+
+export default cartSlice.reducer;

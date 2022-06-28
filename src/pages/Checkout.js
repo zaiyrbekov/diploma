e69cc3c6200a0a52ec080069b68e5e6fa5d7db5b
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-28 11:07:12 +0600

    Add localId to Checkout page

---
+++ src/pages/Checkout.js
@@ -0,0 +1,69 @@
+import Header from "../components/Header/Header";
+import image from "../assets/header.jpg";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { checkout } from "../redux/cartSlice";
+import CartDisplay from "../components/CartDisplay/CartDisplay";
+import { useEffect } from "react";
+
+function Checkout() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { items, localId } = useSelector((store) => ({
+    items: store.cart.items,
+    localId: store.auth.localId,
+  }));
+
+  useEffect(() => {
+    if (!localId) {
+      navigate("/auth");
+    }
+  }, [localId, navigate]);
+
+  function onCheckout(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    dispatch(
+      checkout({
+        localId: localId,
+        items: items,
+        ...Object.fromEntries(formData.entries()),
+      })
+    );
+    navigate("/");
+  }
+
+  return (
+    <>
+      <Header title="Checkout" image={image}></Header>
+      <CartDisplay />
+      <div className="all-cart">
+        <form className="CARTS" onSubmit={onCheckout}>
+          <label>
+            First name:
+            <input type="text" name="firstName" required />
+          </label>
+          <br />
+          <label>
+            Last name:
+            <input type="text" name="lastName" required />
+          </label>
+          <br />
+          <label>
+            Address:
+            <input type="text" name="address" required />
+          </label>
+          <br />
+          <label>
+            Phone:
+            <input type="text" name="phone" required />
+          </label>
+        </form>
+        <button className="btn-checkout">Complete the order</button>
+      </div>
+    </>
+  );
+}
+
+export default Checkout;

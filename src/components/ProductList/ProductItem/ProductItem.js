2e1c73f62dea30268463d0e3a1650b91addbe243
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-28 11:13:21 +0600

    Add new product related component

--- src/components/ProductList/ProductItem/ProductItem.js
+++ src/components/ProductList/ProductItem/ProductItem.js
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
 import classes from "./ProductItem.module.css";
+// import CartIcon from "../../CartButton/CartIcon";
+// import { useParams } from "react-router-dom";
+// import { getProduct } from "../data/products";
 
 function ProductItem({ product }) {
+  // const params = useParams();
+  // const product = getProduct(params.productId);
+
   return (
     <div className={classes.ProductItem}>
       <img src={product.image} alt={product.title} />
@@ -9,6 +15,7 @@ function ProductItem({ product }) {
         <Link to={"/products/" + product.productId}>{product.title}</Link>
         <p>{product.lorem}</p>
         <div className={classes.price}>${product.price}</div>
+        {/* <CartIcon productId={params.productId} /> */}
       </div>
     </div>
   );

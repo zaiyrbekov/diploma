f4536e46cbcd7d0126a142894ef8d91eae9f5614
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-28 11:08:15 +0600

    Add sign in and signout links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import Home from "./pages/Home";
+import Contacts from "./pages/Contacts";
+import Products from "./pages/Products";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Product from "./pages/Product";
 import NotFound from "./pages/NotFound";
-import Products from "./pages/Products";
-import Contacts from "./pages/Contacts";
 import Categories from "./pages/Categories";
 import Category from "./pages/Category";
 import About from "./pages/About";
+import Cart from "./pages/Cart";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import Checkout from "./pages/Checkout";
+import Auth from "./pages/Auth";
+import SignOut from "./pages/SignOut";
 
 function App() {
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch({ type: 'cart/restore' });
+  }, []);
+
   return (
     <div className="App">
       <Layout>
@@ -21,6 +33,10 @@ function App() {
           <Route path="/categories/:categoryId" element={<Category />} />
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/about" element={<About />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/signout" element={<SignOut />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>

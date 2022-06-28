f3c16010d075e338b437271c4c79414d56f63563
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-28 11:15:20 +0600

    Fix signout component issue

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,17 @@
 import classes from "./Nav.module.css";
 import NavItem from "./NavItem/NavItem";
+import { useSelector } from "react-redux";
 
 function Nav() {
+  const isAuthenticated = useSelector(store => store.auth.idToken !== null);
   return (
     <ul className={classes.Nav}>
       <NavItem url="/">Home</NavItem>
       <NavItem url="/products">Menu</NavItem>
       <NavItem url="/contacts">Contacts</NavItem>
       <NavItem url="/about">About</NavItem>
+      { isAuthenticated ? <NavItem url="/signout">Sign out</NavItem> : null }
+      { !isAuthenticated ? <NavItem url="/auth">Sign in</NavItem> : null }
     </ul>
   );
 }

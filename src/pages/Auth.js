837a9306837528456dcdd7612e3fcea6aea2e0d6
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-28 11:09:14 +0600

    Basic auth logic in Auth component

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,17 +1,47 @@
-export default function Auth() {
-  return (
-    <form>
-      <label>
-        Email:
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Password:
-        <input type="password" name="password" />
-      </label>
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { start } from "../redux/authSlice";
 
-      <button>Sign in</button>
-      <button>Sign up</button>
+export default function Auth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function onAuthStart(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    dispatch(start({
+      email: formData.get('email'),
+      password: formData.get('password'),
+      method: event.nativeEvent.submitter.innerText === "Sign up" ? 'signup' : 'signin',
+    }));
+
+    navigate('/');
+  }
+
+
+  return (
+    <form onSubmit={onAuthStart}>
+      <h1 className="Auth-title">Welcome</h1>
+      <div className="Auth">
+        <label>
+          <input type="email" placeholder="E-mail" name="email" required />
+        </label>
+        <label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            required
+            minLength="6"
+          />
+        </label>
+      </div>
+
+      <div className="Button-center">
+        <button className="Button">Sign in</button>
+        <button className="Button">Sign up</button>
+      </div>
     </form>
   );
 }

27ce7a8780f02d5b8803daa2d7739bdfdb4ba3ae
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-28 11:11:28 +0600

    Add localStore to remember the cart items

---
+++ src/components/CartLink/CartLink.js
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import "../CartLink/CartLink.css"
+
+export default function CartLink() {
+  const number = useSelector((store) => {
+    return Object.values(store.cart.items).reduce(
+      (sum, number) => sum + number,
+      0
+    );
+  });
+
+  return (
+    <NavLink className="Cartlink" to="/cart">
+      <div>
+        <span className="CartLinkSpan">
+          <i className="fa-solid fa-cart-shopping CartLink"> </i> {number}
+        </span>
+      </div>
+    </NavLink>
+  );
+}

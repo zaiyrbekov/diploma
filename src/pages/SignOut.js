c580319f3c7fe2d6156a9ef9431bb6223d100e55
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-06-29 10:38:44 +0600

    Add Dispatch and novigate in empty massiv

--- src/pages/SignOut.js
+++ src/pages/SignOut.js
@@ -1,15 +1,15 @@
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-// export default function SignOut() {
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
+export default function SignOut() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-//   useEffect(() => {
-//     dispatch({ type: 'auth/init' });
-//     navigate('/');
-//   }, []);
+  useEffect(() => {
+    dispatch({ type: 'auth/init' });
+    navigate('/');
+  }, [dispatch, navigate]);
 
-//   return 'asd';
-// }
+  return 'asd';
+}
